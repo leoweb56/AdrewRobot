fb1f369cf5743a9891ce40d82c6550111eed50de
Leonardo D'Antoni <lfdantoni@gmail.com>
2013-12-02 13:12:21 -0300

    Inicializacion

---
+++ AndrewRobot/AndrewRobot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Robocode;
+
+//Andrew Robot Team
+namespace ART
+{
+    public class AndrewRobot : Robot
+    {
+        public override void Run()
+        {
+            while (true)
+            {
+                Ahead(5000);
+
+                TurnRight(90);
+            }
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            // We fire the gun with bullet power = 1
+            Fire(1);
+        }
+    }
+}
